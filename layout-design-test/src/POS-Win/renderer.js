dffbcd8aaf80646670c444160426e9839c802361
CARMO_DA_GAMA <carmodagama@gmail.com>
2023-11-27 16:19:08 +0100

    Finished POS System Menu Sibar

--- layout-design-test/src/POS-Win/renderer.js
+++ layout-design-test/src/POS-Win/renderer.js
@@ -9,11 +9,27 @@ function activeLink() {
 
 list.forEach((item) => item.addEventListener("click", activeLink));
 
-let toggle = document.querySelector(".toggle");
+let old_toggle = document.querySelector(".old-toggle");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
 
-toggle.onclick = () => {
+console.log(old_toggle);
+old_toggle.onclick = () => {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  
+  document.getElementById(pageName).style.display = "block";
+}
+
+document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
