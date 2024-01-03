358e74698da28bfe3e9ade8dcd1faaafff70e288
CarmoDaGama <carmodagama@gmail.com>
2024-01-03 15:52:30 +0000

    imp list printers#3

--- layout-design-test/src/assets/js/myHelpers.js
+++ layout-design-test/src/assets/js/myHelpers.js
@@ -1,3 +1,5 @@
+
+
 export class MyNumberFormatter
 {
   defaultFormat(number){
--- layout-design-test/test.js
+++ layout-design-test/test.js
@@ -1,12 +1,28 @@
-function add () {
-    let go ="came";
-    let ha ="Gama";
-    return {go, ha}
+const cmd = async (command) => {
+    const util = require("node:util");
+    const exec = util.promisify(require("node:child_process").exec);
+    const { stdout, stderr } = await exec(command);   
+    return {stderr, stdout};
+};
+
+async function getPrinterList(){  
+    const { stdout, stderr } = await cmd("wmic printer list brief");
+    if (stderr) {
+        return;
+    }
+    var printers = [];
+    
+    let vt_stdout = stdout.split("  ");
+    j = 0;
+    vt_stdout = vt_stdout.filter((item) => item);
+    for (i = 0; i < vt_stdout.length; i++) {
+      if (vt_stdout[i] == " \r\r\n" || vt_stdout[i] == "\r\r\n") {
+        printers[j] = vt_stdout[i + 1];
+        j++;
+      }
+    }
+    return printers;
 }
 
-function print(){
-    let {go, ha} = add();
-    console.log(`${go} and ${ha}`);
-}
+getPrinterList();
 
-print();
